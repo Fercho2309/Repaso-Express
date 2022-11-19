f1526d90f9d923393e402d210cfd764f8c93720b
Fercho2309 <efibarra67@misena.edu.co>
2022-11-19 12:51:50 -0500

    cuarto commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,68 @@
-import { useState } from 'react'
-import './index.css'
-
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
+// Context
+import { AuthProvider } from './context/AuthProvider'
+import { UsuariosProvider } from './context/UsuariosProvider'
+//import { ProductosProvider } from './context/ProductosProvider'
+
 // Layout (LayoutAuth)
-import LayoutAuth from '../Layout/LayoutAuth.jsx'
+import LayoutAuth from './Layout/LayoutAuth'
+import RutaProtegida from './Layout/RutaProtegida'
 
 // pages (usuarios)
-import Login  from './pages/Login.jsx'
-import Registro from './pages/Usuario/Registro.jsx'
-import OlvidePassword from './pages/Usuario/OlvidePassword.jsx'
-import Confirmar from './pages/Usuario/Confirmar.jsx'
+import Login from './pages/Login'
+import Registro from './pages/usuario/Registro'
+import OlvidePassword from './pages/usuario/OlvidePassword'
+import Confirmar from './pages/usuario/Confirmar'
+
+// pages (usuarios protegidas)
+import Perfil from './pages/Usuario/Perfil'
+import CambiarPassword from './pages/usuario/CambiarPassword.jsx'
+
+// pages (productos)
+// import ListaProductos from './pages/productos/ListaProductos'
+// import FormularioProductos from './pages/productos/FormularioProductos'
+// import DetalleProducto from './pages/productos/DetalleProducto'
 
 function App() {
   return (
-  
-  <Router>
 
-    <Routes>
+    <Router>
 
-      {/* RUTAS PUBLICAS */}
-      <Route path='/' element={<LayoutAuth />}>
+      <AuthProvider>
 
-        <Route index element={<Login />} />
-        <Route path='registro' element={<Registro />} />
-        <Route path='olvide-password' element={<OlvidePassword />} />
-        <Route path='confirmar/:id' element={<Confirmar />} />
+        <UsuariosProvider>
 
-      </Route>
+          {/* <ProductosProvider> */}
+          <Routes>
 
-    </Routes>
+              {/* RUTAS PUBLICAS */}
+              <Route path='/' element={<LayoutAuth />}>
+                <Route index element={<Login />} />
+                <Route path='registro' element={<Registro />} />
+                <Route path='olvide-password' element={<OlvidePassword />} />
+                <Route path='confirmar/:id' element={<Confirmar />} />
+              </Route>
 
-  </Router>
+              {/* Rutas Protegidas */}
+              <Route path='/perfil' element={<RutaProtegida />}>
+                <Route index element={<Perfil />} />
+                <Route path="cambiar-password" element={<CambiarPassword />} />
+              </Route>
 
-)
+            </Routes>
+
+          {/* </ProductosProvider> */}
+
+        </UsuariosProvider>
+
+      </AuthProvider>
+
+    </Router>
+
+  )
+
 }
+
+
 export default App
